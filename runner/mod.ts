6ea1c33ab3e13d2c34a16e5edc2597f65934f262
Alexiy Rybin <57483029+tapnisu@users.noreply.github.com>
2023-02-02 18:03:46 +0300

    add warnings and errors to output

--- runner/mod.ts
+++ runner/mod.ts
@@ -2,6 +2,8 @@ const run = (code) => {
     let output = "";
 
     console.log = (args) => output += args.toString() + "\n";
+    console.warn = (args) => output += args.toString() + "\n";
+    console.error = (args) => output += args.toString() + "\n";
 
     eval(code);
 
