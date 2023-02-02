a1e6d1c822fc2fc449fa3ac8fd7a6c8aee964c2b
ND <124185383+user6879@users.noreply.github.com>
2023-02-02 20:40:29 +0500

    Update start.ts

--- actions/start.ts
+++ actions/start.ts
@@ -1,5 +1,5 @@
 import { bot } from "../config/index.ts"
 
 bot.command('start', ctx => {
-    ctx.reply("Hi there, send me a code with /code command.\nFor example:\n<code>let num = 69;\nconsole.log(num);</code>")
+    bot.api.sendMessage(ctx.message.chat.id,"Hi there, send me a code with /code command.\nFor example:\n<code>let num = 69;\nconsole.log(num);</code>",{ parse_mode: "HTML" })
 })
