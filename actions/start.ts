98459709e0acd06e249b6dac0b46b9a72d6995d6
ND <124185383+user6879@users.noreply.github.com>
2023-02-23 20:13:29 +0500

    [fix] my bad

--- actions/start.ts
+++ actions/start.ts
@@ -1,9 +1,5 @@
 import { bot } from "../config/index.ts"
 
 bot.command('start', ctx => {
-<<<<<<< HEAD
     bot.api.sendMessage(ctx.message.chat.id,"Hi there, send me a code with /code command.\nFor example:\n<code>let num = 69;\nconsole.log(num);</code>",{ parse_mode: "HTML" })
-=======
-    ctx.reply("Hi there, send me a code with /code command.\nFor example:\n/code\n<code>let num = 69;\nconsole.log(num);</code>")
->>>>>>> e17c83b (fmt)
 })
